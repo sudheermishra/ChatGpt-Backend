22bb8004ecc698e0ff4dfc47568af102704b6ff4
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-08-10 19:27:46 +0530

    changes: dotenv config & cookierParser for token express.json nad user route

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import dbConnet from "./config/database.js";
+import userRouter from "./routes/userRouter.js";
+import cookieParser from "cookie-parser";
+
 dotenv.config();
+
 const app = express();
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.use("/user", userRouter);
 
 const starServer = async () => {
   try {
