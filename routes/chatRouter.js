f54aeced304a1fbc8ad832aa4923576c31a534b2
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-08-14 11:07:21 +0530

    feat:createChat router

---
+++ routes/chatRouter.js
@@ -0,0 +1,16 @@
+import express from "express";
+import authUserMiddleware from "../middleware/authUserMiddleware.js";
+import { createChat } from "../controllers/chatController.js";
+const chatRouter = express.Router();
+
+chatRouter.use(authUserMiddleware);
+
+chatRouter.post("/createChat", createChat);
+
+// chatRouter.get("/getRecentChat", getRecentChat);
+
+// chatRouter.get("/:chatId", getSingleChat);
+
+// chatRouter.delete("/:chatId", deleteSignleChat);
+
+export default chatRouter;
