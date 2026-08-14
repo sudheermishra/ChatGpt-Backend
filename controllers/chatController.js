a30e7a7f97785039722bdf635c4f9c9076d784ba
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-08-14 11:27:05 +0530

    feat: RecentChat controller function

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -10,7 +10,7 @@ export const createChat = async (req, resp) => {
     }
 
     const chats = await Chat.create({ userId: req.user._id, model: model });
-
+    console.log("chats are :", chats);
     resp.status(201).json({
       message: "Chat Created Successfully",
       chatId: chats._id,
@@ -18,6 +18,24 @@ export const createChat = async (req, resp) => {
       model: model,
       topic: chats.topic,
       createdAt: chats.createdAt,
+    });
+  } catch (error) {
+    console.log(error);
+    resp.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const getRecentChat = async (req, resp) => {
+  try {
+    const chats = await Chat.find({ userId: req.user._id })
+      .select("topic updatedAt model")
+      .sort({ updatedAt: -1 });
+
+    resp.status(200).json({
+      message: " Your All Recents Chats",
+      chats,
     });
   } catch (error) {
     console.log(error);
