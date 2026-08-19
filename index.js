40b1850c997b3c79c8da3cc639ef530be92e0a03
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-08-19 14:55:47 +0530

    fix: initialize dotenv before loading dependent modules

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import express from "express";
-import dotenv from "dotenv";
+import dotenv from "dotenv/config";
 import dbConnet from "./config/database.js";
 import userRouter from "./routes/userRouter.js";
 import cookieParser from "cookie-parser";
 import chatRouter from "./routes/chatRouter.js";
 import messageRouter from "./routes/messageRouter.js";
-dotenv.config();
 
 const app = express();
 
