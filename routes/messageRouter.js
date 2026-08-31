ddde6d331268e71ccff332de6b94845594338e7a
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-08-31 10:03:53 +0530

    feat: apply rate limiter to message routes

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authUserMiddleware from "../middleware/authUserMiddleware.js";
 import { getMessage, sendMessage } from "../controllers/messageController.js";
+import authenticatedUserRateLimiter from "../middleware/authenticatedUserRateLimiter.js";
 const messageRouter = express.Router();
 
 messageRouter.use(authUserMiddleware);
+messageRouter.use(authenticatedUserRateLimiter);
 
 messageRouter.post("/", sendMessage);
 messageRouter.post("/:chatId", sendMessage);
