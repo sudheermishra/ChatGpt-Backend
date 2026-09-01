a0308b10a11a772cb7f1788427255d89127928dc
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-01 11:13:21 +0530

    fix bug

--- config/redis.js
+++ config/redis.js
@@ -4,7 +4,9 @@ const redisClient = createClient({
   url: process.env.REDIS_URL,
 });
 
-redisClient.on(("error", (error) => console.log("Redis Client Error", error)));
+redisClient.on("error", (error) => {
+  console.log("Redis Client Error", error);
+});
 
 const connectRedis = async () => {
   await redisClient.connect();
