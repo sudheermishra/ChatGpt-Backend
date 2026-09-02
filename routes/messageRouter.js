5e14787026148b768b287f0d0dba129dabe4016d
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-02 14:36:33 +0530

    tokenUsageMiddleware and loadUserMiddleware added

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -2,13 +2,20 @@ import express from "express";
 import authUserMiddleware from "../middleware/authUserMiddleware.js";
 import { getMessage, sendMessage } from "../controllers/messageController.js";
 import authenticatedUserRateLimiter from "../middleware/authenticatedUserRateLimiter.js";
+import tokenUsageMiddleware from "../middleware/tokenUsageMiddleware.js";
+import loadUserMiddleware from "../middleware/loadUserMiddleware.js";
 const messageRouter = express.Router();
 
 messageRouter.use(authUserMiddleware);
 messageRouter.use(authenticatedUserRateLimiter);
 
-messageRouter.post("/", sendMessage);
-messageRouter.post("/:chatId", sendMessage);
-messageRouter.get("/:chatId", getMessage);
+messageRouter.post("/", tokenUsageMiddleware, loadUserMiddleware, sendMessage);
+messageRouter.post(
+  "/:chatId",
+  tokenUsageMiddleware,
+  loadUserMiddleware,
+  sendMessage,
+);
+messageRouter.get("/:chatId", loadUserMiddleware, getMessage);
 
 export default messageRouter;
