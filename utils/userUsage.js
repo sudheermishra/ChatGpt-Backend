f8b43633fca055fff4749173c0796d4ad2991b12
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-02 14:32:34 +0530

    Remove the resetToken and tokenlimit

--- utils/userUsage.js
+++ utils/userUsage.js
@@ -1,19 +1,4 @@
-export const tokenLimitReached = async (user) => {
-  return user.usage.tokenUsed >= user.usage.tokenLimit;
-};
-
-export const resetTokenIfNeeded = async (user) => {
-  const now = new Date();
-
-  if (now > user.usage.resetAt) {
-    user.usage.tokenUsed = 0;
-    user.usage.resetAt = new Date(Date.now() + 5 * 60 * 60 * 1000);
-    await user.save;
-  }
-};
-
 export const addUserTokenUsage = async (user, totalTokens) => {
-  user.usage.tokenUsed += totalTokens;
   user.usage.totalTokenUsed += totalTokens;
 
   await user.save();
