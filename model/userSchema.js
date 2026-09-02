8c696a09b6b9d41ab2a27d9beec555f1a9dc85c4
Sudheer Mishra <sudheermishra8587@gmail.com>
2026-09-02 14:33:16 +0530

    remove the tokenlimit and reset

--- model/userSchema.js
+++ model/userSchema.js
@@ -20,18 +20,6 @@ const userSchema = new mongoose.Schema(
       required: true,
     },
     usage: {
-      tokenUsed: {
-        type: Number,
-        default: 0,
-      },
-      tokenLimit: {
-        type: Number,
-        default: 10000,
-      },
-      resetAt: {
-        type: Date,
-        default: () => new Date(Date.now() + 5 * 60 * 60 * 1000),
-      },
       totalTokenUsed: {
         type: Number,
         default: 0,
